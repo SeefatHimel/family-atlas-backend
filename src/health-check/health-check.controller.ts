22166687339f451c0d3e19ca7b8606847c731521
Seefat Hossain Himel <seefathimel1@gmail.com>
2023-11-23 17:57:51 +0600

    init : nest js project

---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheckService } from './health-check.service';
+
+@Controller('health-check')
+export class HealthCheckController {
+  constructor(private readonly healthCheckService: HealthCheckService) {}
+  @Get()
+  async checkHealth() {
+    return await this.healthCheckService.checkHealth();
+  }
+}
