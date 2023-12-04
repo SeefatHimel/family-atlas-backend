7a05818d605e11b1a569d68c55ad6202307be8fc
Seefat Hossain Himel <seefathimel1@gmail.com>
2023-12-04 20:17:17 +0600

    update : prettier settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { HealthCheckModule } from "./health-check/health-check.module";
-import { PrismaModule } from "./prisma/prisma.module";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HealthCheckModule } from './health-check/health-check.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [HealthCheckModule, PrismaModule],
