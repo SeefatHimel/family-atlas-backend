8660b36bf6303431e6bfd38812143d6e369051ff
Seefat Hossain Himel <seefathimel1@gmail.com>
2023-12-04 20:10:03 +0600

    add prettier settings

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,16 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     try {
       await this.$connect();
-      console.log('Database connection successful!');
+      console.log("Database connection successful!");
     } catch (error) {
       console.log(
-        '🚀 ~ file: prisma.service.ts:11 ~ PrismaService ~ onModuleInit ~ error:',
-        error,
+        "🚀 ~ file: prisma.service.ts:11 ~ PrismaService ~ onModuleInit ~ error:",
+        error
       );
     }
   }
